e58941e8210d16e4b0a342c97063db98e1a4cb55
MaximRakhmanin <maxim.r@2muchcoffee.com>
2018-03-29 16:39:21 +0300

    Create Project

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {FormsModule} from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+@NgModule({
+  imports: [
+      CommonModule,
+      FlexLayoutModule,
+      FormsModule,
+      BrowserAnimationsModule,
+  ],
+  declarations: []
+})
+export class SharedModule { }

9d0c9515b7fcc6e0bf5743e7a50b8e1e417ad942
MaximRakhmanin <maxim.r@2muchcoffee.com>
2018-05-17 19:09:03 +0300

    feat(product.reducer) added new payload isLoadProduct

--- src/app/ngrx/product/reducers/product.reducer.ts
+++ src/app/ngrx/product/reducers/product.reducer.ts
@@ -1,17 +1,21 @@
-import * as ProductActions from '../actions/product.actions';
+import * as ProductActions from '../actions';
 import { initialState, IProductsState } from '../states/product.states';
 
 export function productReducer(
   state = initialState,
-  action: ProductActions.Actions): IProductsState {
-  switch (action.type) {
+  {type, payload}: ProductActions.Actions): IProductsState {
+  switch (type) {
     case ProductActions.Get_List_Success_full: {
+
+      const entities = payload.reduce((accEntities, currProduct) =>
+          ({...accEntities, [currProduct.id]: currProduct }), {});
+      const collectionIds = payload.map(product => product.id);
+
       return {
         ...state,
-        entities: action.payload.reduce((entities, currProduct) => {
-          return { ...entities, [currProduct.id]: currProduct };
-        }, {}),
-        collectionIds: action.payload.map(product => product.id)
+        entities,
+        collectionIds,
+        isLoadProduct: true
       };
 }
     default: {

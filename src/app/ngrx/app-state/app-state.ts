5d87ab6d5821cc4eca898949e0676a1b32fa4a6c
MaximRakhmanin <maxim.r@2muchcoffee.com>
2018-05-21 12:55:22 +0300

    feat(app-state) added variable reducers

--- src/app/ngrx/app-state/app-state.ts
+++ src/app/ngrx/app-state/app-state.ts
@@ -1,19 +1,13 @@
 import { IProductsState } from '../product/states';
-import { IProductRequest } from '../requests/nested-states/products/states';
 import { productReducer } from '../product/reducers';
 import { requestReducer } from '../requests/reducers';
-import { combineReducers } from '@ngrx/store';
+import { IRequestState } from '../requests/states';
 
 export interface AppState {
   readonly products: IProductsState;
-  readonly request: IProductRequest;
-  readonly getProduct;
+  readonly requests: IRequestState;
 }
-const reducers = {
+export const reducers = {
   products: productReducer,
-  request: requestReducer,
+  requests: requestReducer,
 };
-const devRed = combineReducers(reducers);
-export function appReducer(state: any, action) {
-  return devRed(state, action);
-}
